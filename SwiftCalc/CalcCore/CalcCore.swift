a3d7a5c3e5ecc065f3b7e472967e88398a599c93
lingostar <skyoon@codershigh.com>
2021-07-15 15:18:51 +0900

    문서화 작업. CalcCore의 기본 레퍼런스 제작
    #18

--- SwiftCalc/CalcCore/CalcCore.swift
+++ SwiftCalc/CalcCore/CalcCore.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
+/// 사칙연산을 위한 연산자 Enumeration.
 public enum CalcOperator: String, Codable {
     case plus
     case minus
     case multiply
     case divide
     
+    /// 연산을 실행할 함수를 가리키는 변수
     var doCalc : (Double , Double) -> Double {
         switch self {
             case .plus:
@@ -26,6 +28,7 @@ public enum CalcOperator: String, Codable {
         }
     }
     
+    /// UI 표시를 위한 연산자의 문자열 표기
     public var symbol : String {
         switch self {
             case .plus:
@@ -40,6 +43,16 @@ public enum CalcOperator: String, Codable {
     }
 }
 
+/**
+계산을 위한 노드.
+ 
+연산자와 연산항의 쌍으로 구성
+
+ ~~~
+ // + : operation , 35 : operand
+ let operation = CalcOperationNode(op: CalcOperator.plus , operand:35.0)
+ ~~~
+*/
 public struct CalcOperationNode: Codable {
     var op: CalcOperator
     var operand : Double
@@ -49,8 +62,8 @@ public struct CalcOperationNode: Codable {
         self.operand = operand
     }
 }
-
-
+//MARK: -
+/// 전체 연산자에 대한 구조체. 베이스 값을 시작으로 operationNode들이 체인처럼 연결된다.
 public struct CalcOperation: Codable {
     public var baseNumber : Double
     public var operationNodes : [CalcOperationNode]
@@ -65,7 +78,11 @@ public struct CalcOperation: Codable {
         self.operationNodes = operationNodes
     }
     
+    /// 우선 순위가 높은 *와 / 연산을 하고 우선 순위가 낮은 +와 - 연산을 리턴한다.
+    ///
+    /// - Returns : + 와 - 로만 구성된 CalcOperation 구조체
     public func mergePriorityNode() -> CalcOperation {
+        // self를 newCalcOperation에 할당하여 새로운 인스턴스를 생성한 뒤, 생성한 인스턴스로 연산을 하고 그 결과를 리턴한다.
         var newCalcOperation = self
         var newNodes:[CalcOperationNode] = []
         
@@ -91,7 +108,9 @@ public struct CalcOperation: Codable {
         return newCalcOperation
     }
     
-    
+    /// 전체 연산을 앞에서부터 순차적으로 수행하고 결과값을 리턴한다
+    ///
+    /// - Returns : 연산의 결과 Double 값
     public func mergeOperationNodes() -> Double {
         let value = operationNodes.reduce(baseNumber, {
             (result:Double, element:CalcOperationNode) in
@@ -100,10 +119,12 @@ public struct CalcOperation: Codable {
         return value
     }
     
+    /// 연산을 순서대로 수행하도록 하는 함수
     public func calcResult() -> Double {
         return mergePriorityNode().mergeOperationNodes()
     }
     
+    /// UI 표시를 위해 연산의 내용을 문자열로 리턴하는 함수
     public func operationString() -> String {
         return operationNodes.reduce(String(baseNumber), {
             (result:String, element:CalcOperationNode) in
